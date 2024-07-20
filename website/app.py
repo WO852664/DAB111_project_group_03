74a364890fd096ba77bc1c7dba37c02b1c2e0c97
Emmanuel Araoyinbo <W0852664@myscc.ca>
2024-07-20 03:57:35 -0400

    website update

--- website/app.py
+++ website/app.py
@@ -1,36 +1,28 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import pandas as pd
 import sqlite3
 
 app = Flask(__name__)
 
-
 def load_data():
     conn = sqlite3.connect('immigration_data.db')
-    query = "SELECT * FROM data_2019 LIMIT 10;"  
+    query = "SELECT * FROM data_2019 LIMIT 10;"  # Limiting to 10 rows for display
     data = pd.read_sql(query, conn)
     conn.close()
     return data
 
+@app.route('/')
+def home():
+    return '<h1>Welcome to the Immigration Data Website</h1><p><a href="/about">About</a> | <a href="/data">Data</a></p>'
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 @app.route('/data')
 def data():
     data = load_data()
     return render_template('data.html', tables=[data.to_html(classes='data')], titles=data.columns.values)
 
-
-@app.route('/')
-def home():
-    return '<h1>Welcome to GROUP 3 PYTHON DATA ON the Immigration Data Website</h1><p><a href="/about">About</a> | <a href="/data">Data</a></p>'
-
-#if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-
-python app.py
-
-
