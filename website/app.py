4f9d461193dc354fcf22efb177e739a32a2d294e
Emmanuel Araoyinbo <W0852664@myscc.ca>
2024-07-20 23:51:49 -0400

    website

--- website/app.py
+++ website/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
 import pandas as pd
 import sqlite3
+import pathlib
+
+
+base_path = pathlib.Path(r'C:\Users\aranu\OneDrive\Desktop\PYTHON\DAB111_project_group_03\database')
+db_name = base_path / 'immigration_data.db'
 
 app = Flask(__name__)
 
 def load_data():
-    conn = sqlite3.connect('immigration_data.db')
-    query = "SELECT * FROM data_2019 LIMIT 10;"  # Limiting to 10 rows for display
-    data = pd.read_sql(query, conn)
-    conn.close()
+    con = sqlite3.connect(db_name)
+    query = "SELECT * FROM data_2019 LIMIT 10;"
+    data = pd.read_sql(query, con)
+    con.close()
     return data
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to the Immigration Data Website</h1><p><a href="/about">About</a> | <a href="/data">Data</a></p>'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -22,7 +27,8 @@ def about():
 @app.route('/data')
 def data():
     data = load_data()
-    return render_template('data.html', tables=[data.to_html(classes='data')], titles=data.columns.values)
+    # Pass the HTML of the dataframe to the template
+    return render_template('data.html', tables=[data.to_html(classes='data', index=False)], titles=data.columns.values)
 
 if __name__ == '__main__':
     app.run(debug=True)
